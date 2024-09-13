ecc50849a8fefdccf37f8fc0ede72a5b2a1233c6
aaimran236 <ai2015236@gmail.com>
2024-09-14 03:35:53 +0600

    weather based on user location

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,12 +1,27 @@
 package com.example.weatherapp;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
@@ -21,13 +36,30 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.Priority;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.squareup.picasso.Picasso;
+import android.Manifest;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     private RelativeLayout homeRL;
@@ -39,8 +71,14 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<WeatherRVModel> weatherRVModelArrayList;
     private WeatherRVAdapter weatherRVAdapter;
     private int PERMISSION_CODE = 1;
-    private String cityName;
+    private static String cityName;
     private AlertDialog.Builder alertdialogBuilder;
+    private FusedLocationProviderClient fusedLocationClient;
+    private LocationCallback locationCallback;
+    private ActivityResultLauncher<String[]> locationPermissionRequest;
+    private static final int REQUEST_CHECK_SETTINGS = 1001;
+    private static boolean granted=false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,11 +100,9 @@ public class MainActivity extends AppCompatActivity {
         weatherRVModelArrayList = new ArrayList<>();
         weatherRVAdapter = new WeatherRVAdapter(this, weatherRVModelArrayList);
         weatherRV.setAdapter(weatherRVAdapter);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
-        cityName = "sylhet";
-        cityNameTV.setText(cityName);
-        if (isNetworkAvailable(MainActivity.this)) getWeatherInfo(cityName);
-        else {
+        if (!isNetworkAvailable(this)){
             alertdialogBuilder = new AlertDialog.Builder(MainActivity.this);
             alertdialogBuilder.setTitle(R.string.title_string);
             alertdialogBuilder.setMessage(R.string.title_message);
@@ -74,13 +110,49 @@ public class MainActivity extends AppCompatActivity {
             alertdialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
-                    finish();
+                    dialogInterface.dismiss();
+
                 }
             });
             AlertDialog alertDialog = alertdialogBuilder.create();
             alertDialog.show();
+            return;
         }
 
+        if (!isLocationEnabled()){
+            checkLocationSettings();
+        }
+
+        locationPermissionRequest =
+                registerForActivityResult(new ActivityResultContracts
+                                .RequestMultiplePermissions(), result -> {
+                            Boolean fineLocationGranted = result.getOrDefault(
+                                    Manifest.permission.ACCESS_FINE_LOCATION, false);
+                            Boolean coarseLocationGranted = result.getOrDefault(
+                                    Manifest.permission.ACCESS_COARSE_LOCATION,false);
+                            if (fineLocationGranted != null && fineLocationGranted) {
+                                // Precise location access granted.
+                                Toast.makeText(this, "Precise location access granted.", Toast.LENGTH_SHORT).show();
+
+                            } else if (coarseLocationGranted != null && coarseLocationGranted) {
+                                // Only approximate location access granted.
+                                Toast.makeText(this, "Only approximate location access granted.", Toast.LENGTH_SHORT).show();
+
+                            } else {
+
+                                // No location access granted.
+                            }
+                        }
+                );
+
+
+        cityName="London";
+
+        cityNameTV.setText(cityName);
+        getWeatherInfo(cityName);
+
+
+        locationPermission();
         searchIV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -96,12 +168,145 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+
+    private boolean isLocationEnabled() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        // Check if GPS or Network Provider is enabled
+        boolean gpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        boolean networkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        return gpsEnabled || networkEnabled;
+    }
+
+    private void locationPermission() {
+        locationPermissionRequest.launch(new String[] {
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        });
+    }
+
+    private void callbackCurrentLocation() {
+
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                // Handle the current location here
+                for (Location location : locationResult.getLocations()) {
+                    if (location != null) {
+                        double latitude = location.getLatitude();
+                        double longitude = location.getLongitude();
+                        getCityName(latitude,longitude);
+                        // Stop updates after receiving the current location (optional)
+                        fusedLocationClient.removeLocationUpdates(this);
+                    }else {
+                        Toast.makeText(MainActivity.this, "location is null", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            }
+        };
+        // Start the process of getting location updates
+        requestCurrentLocation();
+    }
+
+    private void requestCurrentLocation() {
+
+        LocationRequest locationRequest = new LocationRequest.Builder(
+                Priority.PRIORITY_HIGH_ACCURACY, 1000) // 10 seconds interval
+                .setMinUpdateIntervalMillis(500)       // Fastest interval of 5 seconds
+                .setMaxUpdates(1)                       // Get only one update for current location
+                .build();
+
+        // Check for location permissions
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED  | ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+
+            // Request a single location update (current location)
+            fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
+
+        } else {
+            // Request location permission if not granted
+            locationPermission();
+        }
+
+    }
+
+
+    private void getCityName(double latitude, double longitude) {
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        try {
+            List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 1);
+            if (addresses != null && !addresses.isEmpty()) {
+                String cityName = addresses.get(0).getLocality();
+               getWeatherInfo(cityName);
+            } else {
+                Toast.makeText(this, "City not found" ,Toast.LENGTH_SHORT).show();
+            }
+        } catch (IOException e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
     public static boolean isNetworkAvailable(Context context) {
         return ((ConnectivityManager) context
                 .getSystemService(Context.CONNECTIVITY_SERVICE))
                 .getActiveNetworkInfo() != null;
     }
 
+    private void checkLocationSettings() {
+
+        LocationRequest locationRequest = new LocationRequest.Builder(
+                Priority.PRIORITY_HIGH_ACCURACY, 10000) // 1 seconds interval
+                .setMinUpdateIntervalMillis(5000)       // Fastest interval of .5 seconds
+                .build();
+
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+
+
+        SettingsClient settingsClient = LocationServices.getSettingsClient(this);
+
+
+        Task<LocationSettingsResponse> task = settingsClient.checkLocationSettings(builder.build());
+
+
+        task.addOnSuccessListener(this, locationSettingsResponse -> {
+            granted=true;
+        });
+
+        task.addOnFailureListener(this, e -> {
+            if (e instanceof ResolvableApiException) {
+                try {
+                    // Show the dialog prompting the user to enable location settings
+                    ResolvableApiException resolvable = (ResolvableApiException) e;
+                    resolvable.startResolutionForResult(this, REQUEST_CHECK_SETTINGS);
+                } catch (IntentSender.SendIntentException sendEx) {
+                    // Handle the error
+                    sendEx.printStackTrace();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CHECK_SETTINGS) {
+            if (resultCode == Activity.RESULT_OK) {
+                // User enabled location settings, proceed to get current location
+                callbackCurrentLocation();
+            } else {
+                // User did not enable location services
+                Toast.makeText(this, "Location services are required for this feature", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
     //method to get weather information of a specific city that user searched for
     private void getWeatherInfo(String cityName) {
@@ -176,7 +381,6 @@ public class MainActivity extends AppCompatActivity {
         });
         AlertDialog alertDialog = alertdialogBuilder.create();
         alertDialog.show();
-
     }
 
 }
